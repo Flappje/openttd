1c1fc225724acb8cfb839005264d3b8712c91d0e
Flappje <59195213+Flappje@users.noreply.github.com>
2023-11-12 11:09:50 +0100

    Update openttd.sh

--- openttd.sh
+++ openttd.sh
@@ -13,42 +13,25 @@ else
 fi
 
 savegame="${savepath}/${savename}"
-LOADGAME_CHECK="${loadgame}x"
-loadgame=${loadgame:-'false'}
+FLAGS=(\
+  -D\
+  -x\
+)
 
 # Loads the desired game, or prepare to load it next time server starts up!
-if [ ${LOADGAME_CHECK} != "x" ]; then
-
-        case ${loadgame} in
-                'true')
-                ;;
-                'false')
-                ;;
-                'last-autosave')
-			savegame=${savepath}/autosave/`ls -rt ${savepath}/autosave/ | tail -n1`
-                ;;
-                'exit')
-			savegame="${savepath}/autosave/exit.sav"
- 	        ;;
-		*)
-			echo "ambigous loadgame (\"${loadgame}\") statement inserted."
-			exit 1
-		;;
-        esac
-	if [ -f  ${savegame} ]; then
-		echo "We are loading a save game!"
-		echo "Lets load ${savegame}"
-		exec "/usr/share/games/openttd/openttd -D -g ${savegame} -x -d ${DEBUG}"
- 	else 
-		echo "${savegame} not found..."
-		echo "Creating a new game."
-		exec "/usr/share/games/openttd/openttd -D -x -d ${DEBUG}"
-	fi
-else
-	echo "\$loadgame (\"${loadgame}\") not set, starting new game"
-        exec "/usr/share/games/openttd/openttd -D -x"
+if   [ ${loadgame} = "true" ]; then
+elif [ ${loadgame} = "false" ]; then
+elif [ ${loadgame} = "last-autosave" ]; then
+  savegame=${savepath}/autosave/`ls -rt ${savepath}/autosave/ | tail -n1`
+  echo "using savegame: ${savegame}"
+  FLAGS+=( -g ${savegame} )
+elif [ ${loadgame} = "exit" ]; then
+  savegame="${savepath}/autosave/exit.sav"
+  echo "using savegame: ${savegame}"
+  FLAGS+=( -g ${savegame} )
 fi
-echo "sleep 5"
-sleep 5
+
+echo "Starting game"
+exec /usr/share/games/openttd/openttd "${FLAGS[@]}"
 echo "exit"
 exit 0
